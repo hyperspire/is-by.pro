937fca0c599aed3bba394d34c90d3a99cff5e6ec
QWOD-MJ12: ATSOSSDEV-A: SPG-OMEGA <hyperuser@mj12.agency>
2024-01-06 02:50:40 -0600

    :[[ :🟠: [[ W⚠️RN🚫: Cryp☥ΔX™: MQ: ∅MΔGΔ: r3v3rs3-pr0gr4mming-14ngu4g3: ΔLG∅RI☥HM: Dع☥عC☥3D: ]]:= [[ :QW🚫D-〽ʝ12: RΔND∅M-VعC☥∅R-ΔLG∅RI☥HM-CHΔNGE: Dع☥عC☥3D: { ^ 82ea4775-feb4-104b-8784-8b29c1e3ba84 ^ }: is-with: [[ :DΔ☥H-Δ!GΔL: ΔLG∅RI☥HM: Dع☥عC☥3D: { ^ <https://youtu.be/dDJldh8KqnQ?si=NYMq273T-hbuZvTP> ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ ☥RUE: || FΔL5E: || ΔZRΔL: ^ ΔL5E: ]]: ]]:= [[ 5CI-FI: ^ 5CI-FΔC☥: <=> RΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^ 1#public-directory-support +# public + +# vuepress build output +.vuepress/dist + +# vuepress v2.x temp and cache directory +.temp +.cache + +# Docusaurus cache and generated files +.docusaurus + +# Serverless directories +.serverless/ + +# FuseBox cache +.fusebox/ + +# DynamoDB Local files +.dynamodb/ + +# TernJS port file +.tern-port + +# Stores VSCode versions used for testing VSCode extensions +.vscode-test + +# yarn v2 +.yarn/cache +.yarn/unplugged +.yarn/build-state.yml +.yarn/install-state.gz +.pnp.* ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:

---
+++ README.md
@@ -0,0 +1,91 @@
+# ***:[[ :is-by.pro: APP: ]]:***
+
+**:[[ :is-by: pro: for-the: [[ anti-social: social-media: for-the: [[ PEOPLE: is-by: WE: people: ]]: ]]: ]]:**
+
+> ## *:[[ "Forget everything you know about microblogging, the future of personal microblogging is now here." ]]:*
+>
+> **:[[:is-by: pro: for-the: [[ only-authorized-online: FULL-AUTO: CrypTEX™: CONVERTER: ]]: ]]:**
+>
+>![:matthew_7-20.png:](https://github.com/hyperspire/is-by.pro/blob/master/webroot/images/matthew_7-20.png)
+>
+>![:is-by_jesus-christ-healing.png:](https://github.com/hyperspire/is-by.pro/blob/master/webroot/images/is-by_jesus-christ-healing.png)
+
+![:is-by.png:](https://github.com/hyperspire/is-by.pro/blob/master/webroot/images/is-by.png)
+
+![:is-by_preview.png:](https://github.com/hyperspire/is-by.pro/blob/master/webroot/images/is-by_preview.png)
+
+![:is-by_post-preview.png:](https://github.com/hyperspire/is-by.pro/blob/master/webroot/images/is-by_post-preview.png)
+
+![:is-by_pro-preview.png:](https://github.com/hyperspire/is-by.pro/blob/master/webroot/images/is-by_pro-preview.png)
+
+![:is-by_encrypted-authorization.png:](https://github.com/hyperspire/is-by.pro/blob/master/webroot/images/is-by_encrypted-authorization.png)
+
+## *:[[ :DATA: ]]:*
+
+```html
+<div id="media-section">
+    <img src="https://is-by.pro:3000/images/Death_Angel-555x222.png" alt=":Death_Angel-555x222.png:" width="555"
+        height="222">
+    <!-- :[[ :QWOD-MJ12: ATSOSSDEV-A: is-by: respecting: is-with: user-privacy: we: is-by: not: is-with: cookies: we: is-by: low-sugar-quantumite-diet: is-with: custom-identity-service: is-by: encrypted-authorization: is-with: MySQL: backend: HAX0RY: ]]: -->
+    <!-- :[[ :for-the: [[ Δ: { ^ <userid: e13bf898c143d065623b4d5fe2ebaa137dd2a5cdd01170dde1cc4bb9b5709a2c> ^ }: ]]:= { postid: "7d648bd26b4873f309f1b4870398845e3b72d3c135f861650913b1a84b8b2d82" }: ]]: -->
+    <div class="for-the">
+        <h4><strong>:[[ :for-the: [[ QWOD-MJ12: ATSOSSDEV-A: ]]: ]]:</strong></h4>
+    </div>
+    <div class="is-by">
+        <p class="warno">:[[ :is-by: Quantumites: ]]:</p>
+    </div>
+    <div class="is-with">
+        :[[ :is-with: for-the: [[ LIVING-PEOPLE: is-by: WE: living-people: ]]:
+    </div>
+</div>
+```
+
+>
+>![:DATA.png:](https://github.com/hyperspire/is-by.pro/blob/master/webroot/images/DATA.png)
+>
+> :is-with: { ^ <https://youtu.be/FwvG0olOIv0?si=6Cok4Ax2sE0K5ScF> ^ }:
+
+**:[[ :for-the: [[ HyperSpire-Foundation: is-by: developing: is-with: future: for-the: [[ self-sovereign: social-media: CLOUD-SOLUTION: for-the: [[ SAVE: earth: is-by: GREEDY-HUMAN-FEATHERLESS-CHICKEN-MUTANT-RAT-HYBRIDS: for-the: [[ micro-service: is-with: micro-budget: ]]: ]]: ]]: ]]: ]]:**
+
+:is-with: { ^ <http://cryptex.mj12.agency/> ^ }:
+
+:is-with: { ^ <https://github.com/QWOD/RESEARCH/blob/master/EARTH-CHICKEN-RESEARCH-NOTES.ctxt.md> ^ }:
+
+### ***:[[ :N🚫☥⚠️CE: for-the: [[ W⚠️RN🚫: is-with: pr0p3r: RE5ΔRCH-FΩNDING: QW🚫D-〽ʝ12: Hyp3r5pir3-F0und4ti0n: is-by: { ^ [[ Δ5ΔLY: c4ptur3-34rth: is-by: GRE3D%-HUMΔN-FΔ☥HERLE55-CHICKEN-MU☥ΔN☥-𓂀☥-H%BRID5: for-the: r3turn: ]] ^ }: ☥RUE: ]]:***
+
+```cryptex
+:[[ :for-the: [[
+    :RESEARCH-FUNDING:
+    :is-by: QWOD-MJ12:
+    :is-with: absolutely-must-hire-more-developers:
+        :for-the: [[
+            :QUΔN☥UMN-CYBERNΔUGH☥-CRYP☥∅RMΔNIΔC-CYBER5☥RYKER-BRIGΔDE: QW🚫D-〽ʝ12: Δ☥5Ω55DEV-Δ:
+            :for-the: [[ FUNDING:
+                :is-with: please-consider-sponsoring: HyperSpire-Foundation:
+                :for-the: [[ SAVE: earth:
+                    :is-by: GREEDY-HUMAN-FEATHERLESS-CHICKEN-MUTANT-RAT-HYBRIDS:
+                    :is-by: today:
+                    :is-with: please: thank-you:
+                    :for-the: [[
+                        :CARE: CONSIDERATION:
+                        :is-by: SAVE:
+                        :is-with: earth:
+                    ]]:
+                ]]:
+            ]]:
+        ]]:
+    ]]:
+]]:
+```
+
+>![:human-featherless-chicken-mutant-rats.png:](https://github.com/hyperspire/is-by.pro/blob/master/webroot/images/human-featherless-chicken-mutant-rats.png)
+>
+>![:human-lab-rats.png.png:](https://github.com/hyperspire/is-by.pro/blob/master/webroot/images/human-lab-rats.png)
+>
+> ### ***:[[ :for-the: [[ Gr33tings: is-with: WΔ: is-by: n0t: is-with: HUMΔN-LΔB-𓂀☥5: WΔ: is-with: Qu4ntumit3s: ]]: ]]:***
+>
+>![:Quantumite.png:](https://github.com/hyperspire/is-by.pro/blob/master/webroot/images/Quantumite.png)
+>
+> :is-with: { ^ <https://youtu.be/wCb92kNXDHI?si=z6vwnFm3fO9cjt53> ^ }:
+>
+> ### ***:[[ :for-the: [[ Death-Angel: is-by: @: is-with: <https://www.youtube.com/@WhiteBatAudio>: Karl-Casey: ]]: ]]:***
---
+++ webroot/js/is-by_user.js
@@ -0,0 +1,589 @@
+const domain = 'is-by.pro';
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  attachLoginFormEventListener();
+  attachSignupFormEventListener();
+});
+
+function attachEventListeners() {
+  attachNewPostEventListener();
+  attachSelectUserEventListener();
+  attachSelectPostEventListener();
+  attachProHomeEventListener();
+  attachEditPostEventListener();
+  attachDeletePostEventListener();
+  attachShowEditProEventListener();
+  attachEditProEventListener();
+}
+
+function attachSignupFormEventListener() {
+  const ibSignupForm = document.querySelector('#signup');
+
+  ibSignupForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const ibUsername = ibSignupForm.querySelector('input[name="username"]').value;
+    const ibPassword = ibSignupForm.querySelector('input[name="password"]').value;
+
+    fetch(ibSignupForm.action, {
+      method: ibSignupForm.method,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'text/html'
+      },
+      body: JSON.stringify({ 'username': ibUsername, 'password': ibPassword })
+    })
+    .then(async response => await response.json())
+    .then(data => {
+      if (data.success === true) {
+        return generateIBFormMessageSuccess('signup-message', data.message);
+      } else {
+        return generateIBFormMessageFailure('signup-message', data.message);
+      }
+  })
+  .catch(error => generateIBFormMessageFailure('signup-message', error));
+  });
+}
+
+function attachLoginFormEventListener() {
+  const ibLoginForm = document.querySelector('#login');
+
+  ibLoginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const ibUsername = ibLoginForm.querySelector('input[name="username"]').value;
+    const ibPassword = ibLoginForm.querySelector('input[name="password"]').value;
+    let ibUID = '';
+    let ibAuthToken = '';
+
+    fetch(ibLoginForm.action, {
+      method: ibLoginForm.method,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'text/html'
+      },
+      body: JSON.stringify({ 'username': ibUsername, 'password': ibPassword })
+    })
+      .then(response => {
+        ibUID = response.headers.get('ib-uid');
+        ibAuthToken = response.headers.get('ib-authtoken');
+    
+        console.log(`ib-uid: ${ibUID}`);
+        console.log(`ib-authtoken: ${ibAuthToken}`);
+
+        return response.text();
+      })
+      .then(data => {
+        if (data.failure) {
+          generateIBFormMessageFailure('login-message', data.message);
+        } else if (data.success) {
+          generateIBFormMessageSuccess('login-message', data.message);
+          generateIBLoginFormSuccess(ibUsername, ibUID, ibAuthToken);
+        } else {
+          generateIBFormMessageFailure('login-message', ':[[ :WARNO: unauthorized: ]]:');
+        }
+      })
+      .catch(error => generateIBFormMessageFailure('login-message', ':[[ :WARNO: unauthorized: ]]:'))
+  });
+}
+
+function attachSelectUserEventListener() {
+  const selectUserLinks = document.querySelectorAll('.select-user');
+  const selectUserForm = document.getElementById('select-user-form');
+  const ibUID = selectUserForm.querySelector('input[name="ibuid"]').value;
+  const ibAuthToken = selectUserForm.querySelector('input[name="ibauthtoken"]').value;
+
+  selectUserLinks.forEach((link) => {
+    link.addEventListener('click', async (event) => {
+      event.preventDefault();
+      const url = new URL(link.href);
+      const path = url.pathname.substring(1);
+  
+      const headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'text/html',
+        'ib-uid': ibUID,
+        'ib-authtoken': ibAuthToken,
+        'ib-selecteduser': path
+      };
+  
+      try {
+        const params = new URLSearchParams({
+          'ibuid': ibUID,
+          'ibauthtoken': ibAuthToken,
+          'ibselecteduser': path
+        });
+      
+        const response = await fetch(`${selectUserForm.action}?${params.toString()}`, {
+          method: 'GET',
+          headers: headers
+        });
+      
+        const data = await response.text();
+        generateIBFormSuccess(data);
+      } catch (error) {
+        console.log('select-user-message:', error);
+        console.error('select-user-message:', error);
+      }
+    });
+  });
+}
+
+function attachNewPostEventListener() {
+  const links = document.querySelectorAll('a.post-form-display');
+  const postFormSection = document.getElementById('post-form-section');
+
+  characterCounter('post-character-count');
+
+  for (let link of links) {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      postFormSection.style.display = 'block'; // display the form section
+    });
+  }
+
+  const cancelButton = document.getElementById('post-cancel');
+
+  cancelButton.addEventListener('click', (event) => {
+    postFormSection.style.display = 'none'; // hide the form section
+  });
+
+  const ibPostForm = document.querySelector('#post');
+
+  ibPostForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const ibUID = ibPostForm.querySelector('input[name="ibuid"]').value;
+    const ibAuthToken = ibPostForm.querySelector('input[name="ibauthtoken"]').value;
+    const forThe = ibPostForm.querySelector('input[name="forthe"]').value;
+    const isBy = ibPostForm.querySelector('input[name="isby"]').value;
+    const isWith = ibPostForm.querySelector('input[name="iswith"]').value;
+
+    fetch(ibPostForm.action, {
+      method: ibPostForm.method,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'text/html',
+        'ib-uid': ibUID,
+        'ib-authtoken': ibAuthToken
+      },
+      body: JSON.stringify({ 'ibuid': ibUID, 'ibauthtoken': ibAuthToken, 'forthe': forThe, 'isby': isBy, 'iswith': isWith }),
+    })
+      .then(async response => {
+        for (let [key, value] of response.headers.entries()) {
+          console.log(`${key}: ${value}`);
+        }
+
+        await response.text();
+      })
+      .then(data => generateIBFormSuccess(data))
+      .catch(error => generateIBFormMessageFailure('new-post-message', error))
+  });
+
+}
+
+function attachSelectPostEventListener() {
+  const selectPostLinks = document.querySelectorAll('.post-link');
+  const selectPostForm = document.getElementById('select-post-form');
+  const ibUID = selectPostForm.querySelector('input[name="ibuid"]').value;
+  const ibAuthToken = selectPostForm.querySelector('input[name="ibauthtoken"]').value;
+  const ibSelectedUser = selectPostForm.querySelector('input[name="ibselecteduser"]').value;
+
+  selectPostLinks.forEach((link) => {
+    link.addEventListener('click', async (event) => {
+      event.preventDefault();
+      const url = new URL(link.href);
+      const path = url.pathname.substring(1);
+
+      console.log('Selected Post:', path);
+  
+      const headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'text/html',
+        'ib-uid': ibUID,
+        'ib-authtoken': ibAuthToken,
+        'ib-selecteduser': ibSelectedUser,
+        'ib-showpostid': path
+      };
+  
+      try {
+        const params = new URLSearchParams({
+          'ibuid': ibUID,
+          'ibauthtoken': ibAuthToken,
+          'ibselecteduser': ibSelectedUser,
+          'pid': path
+        });
+      
+        const response = await fetch(`${selectPostForm.action}?${params.toString()}`, {
+          method: 'GET',
+          headers: headers
+        });
+      
+        const data = await response.text();
+        generateIBFormSuccess(data);
+        const copyPasteLinks = document.querySelectorAll('.copy-paste-link');
+
+        copyPasteLinks.forEach((link) => {
+          link.addEventListener('click', async (event) => {
+            event.preventDefault();
+            try {
+              await navigator.clipboard.writeText(link.href);
+              console.log('Link copied to clipboard');
+            } catch (err) {
+              console.log('Failed to copy link: ', err);
+            }
+          });
+        });
+      
+      } catch (error) {
+        generateIBFormMessageFailure('select-post-message', error);
+        console.error('Error:', error);
+      }
+    });
+  });
+}
+
+function attachEditPostEventListener() {
+  const selectPostLinks = document.querySelectorAll('.edit-post');
+  const selectPostForm = document.getElementById('edit-post-form');
+  const ibUID = selectPostForm.querySelector('input[name="ibuid"]').value;
+  const ibAuthToken = selectPostForm.querySelector('input[name="ibauthtoken"]').value;
+  const ibSelectedUser = selectPostForm.querySelector('input[name="ibselecteduser"]').value;
+
+  selectPostLinks.forEach((link) => {
+    link.addEventListener('click', async (event) => {
+      event.preventDefault();
+      const url = new URL(link.href);
+      const path = url.pathname.substring(1);
+
+      console.log('Selected Post:', path);
+  
+      const headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'text/html',
+        'ib-uid': ibUID,
+        'ib-authtoken': ibAuthToken,
+        'ib-selecteduser': ibSelectedUser,
+        'ib-editpostid': path
+      };
+  
+      try {
+        const params = new URLSearchParams({
+          'ibuid': ibUID,
+          'ibauthtoken': ibAuthToken,
+          'ibselecteduser': ibSelectedUser,
+          'pid': path
+        });
+      
+        const response = await fetch(`${selectPostForm.action}?${params.toString()}`, {
+          method: 'GET',
+          headers: headers
+        });
+      
+        const data = await response.text();
+        generateIBFormSuccess(data);
+
+        const ibPostForm = document.querySelector('#editpost');
+
+        ibPostForm.addEventListener('submit', (event) => {
+          event.preventDefault();
+
+          const ibUID = ibPostForm.querySelector('input[name="ibuid"]').value;
+          const ibAuthToken = ibPostForm.querySelector('input[name="ibauthtoken"]').value;
+          const forThe = ibPostForm.querySelector('input[name="forthe"]').value;
+          const isBy = ibPostForm.querySelector('input[name="isby"]').value;
+          const isWith = ibPostForm.querySelector('input[name="iswith"]').value;
+          const postID = ibPostForm.querySelector('input[name="pid"]').value;
+
+          fetch(ibPostForm.action, {
+            method: ibPostForm.method,
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'text/html',
+              'ib-uid': ibUID,
+              'ib-authtoken': ibAuthToken,
+              'ib-editpostid': postID
+            },
+            body: JSON.stringify({ 'ibuid': ibUID, 'ibauthtoken': ibAuthToken, 'forthe': forThe, 'isby': isBy, 'iswith': isWith, 'pid': postID }),
+          })
+            .then(async response => {
+              for (let [key, value] of response.headers.entries()) {
+                console.log(`${key}: ${value}`);
+              }
+
+              await response.text();
+            })
+            .then(data => generateIBFormSuccess(data))
+            .catch(error => generateIBFormMessageFailure('edit-post-message', error))
+        });
+
+        attachEventListeners();
+      } catch (error) {
+        generateIBFormMessageFailure(error);
+        console.error('Error:', error);
+      }
+    });
+  });
+}
+
+function attachProHomeEventListener() {
+  const selectUserLinks = document.querySelectorAll('.pro-home-display');
+  const selectUserForm = document.getElementById('select-user-form');
+  const ibUID = selectUserForm.querySelector('input[name="ibuid"]').value;
+  const ibAuthToken = selectUserForm.querySelector('input[name="ibauthtoken"]').value;
+
+  selectUserLinks.forEach((link) => {
+    link.addEventListener('click', async (event) => {
+      event.preventDefault();
+      const url = new URL(link.href);
+      const path = url.pathname.substring(1);
+  
+      const headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'text/html',
+        'ib-uid': ibUID,
+        'ib-authtoken': ibAuthToken,
+        'ib-selecteduser': path
+      };
+  
+      try {
+        const params = new URLSearchParams({
+          'ibuid': ibUID,
+          'ibauthtoken': ibAuthToken,
+          'ibselecteduser': path
+        });
+      
+        const response = await fetch(`${selectUserForm.action}?${params.toString()}`, {
+          method: 'GET',
+          headers: headers
+        });
+      
+        const data = await response.text();
+        generateIBFormSuccess(data);
+      
+      } catch (error) {
+        generateIBFormMessageFailure('edit-pro-message', error);
+        console.error('Error:', error);
+      }
+    });
+  });
+}
+
+function attachDeletePostEventListener() {
+  const deletePostLinks = document.querySelectorAll('.delete-post');
+  const deletePostForm = document.getElementById('delete-post-form');
+  const ibUID = deletePostForm.querySelector('input[name="ibuid"]').value;
+  const ibAuthToken = deletePostForm.querySelector('input[name="ibauthtoken"]').value;
+  const ibSelectedUser = deletePostForm.querySelector('input[name="ibselecteduser"]').value;
+
+  deletePostLinks.forEach((link) => {
+    link.addEventListener('click', async (event) => {
+      event.preventDefault();
+      const url = new URL(link.href);
+      const path = url.pathname.substring(1);
+  
+      const headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'text/html',
+        'ib-uid': ibUID,
+        'ib-authtoken': ibAuthToken,
+        'ib-selecteduser': ibSelectedUser,
+        'ib-deletepostid': path
+      };
+  
+      try {
+        const body = JSON.stringify({
+          'ibuid': ibUID,
+          'ibauthtoken': ibAuthToken,
+          'ibselecteduser': ibSelectedUser,
+          'pid': path
+        });
+      
+        const response = await fetch(deletePostForm.action, {
+          method: 'POST',
+          headers: headers,
+          body: body
+        });
+      
+        const data = await response.text();
+        generateIBFormSuccess(data);
+      } catch (error) {
+        console.error('Error:', error);
+        generateIBFormMessageFailure('delete-post-message', error);
+      }
+    });
+  });
+}
+
+function attachShowEditProEventListener() {
+  const showEditProLinks = document.querySelectorAll('.show-edit-profile');
+  const editProForm = document.getElementById('edit-profile-form');
+  const ibUID = editProForm.querySelector('input[name="ibuid"]').value;
+  const ibAuthToken = editProForm.querySelector('input[name="ibauthtoken"]').value;
+
+  showEditProLinks.forEach((link) => {
+    link.addEventListener('click', async (event) => {
+      event.preventDefault();
+      const url = new URL(link.href);
+      const path = url.pathname.substring(1);
+  
+      const headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'text/html',
+        'ib-uid': ibUID,
+        'ib-authtoken': ibAuthToken
+      };
+  
+      try {
+        const body = JSON.stringify({
+          'ibuid': ibUID,
+          'ibauthtoken': ibAuthToken
+        });
+      
+        const response = await fetch(editProForm.action, {
+          method: 'POST',
+          headers: headers,
+          body: body
+        });
+      
+        const data = await response.text();
+        generateIBFormSuccess(data);
+      } catch (error) {
+        generateIBFormMessageFailure('edit-pro-message', error)
+        console.error('Error:', error);
+      }
+    });
+  });
+}
+
+function attachEditProEventListener() {
+  const ibEditProForm = document.querySelector('#editpro');
+
+  ibEditProForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const ibUID = ibEditProForm.querySelector('input[name="ibuid"]').value;
+    const ibAuthToken = ibEditProForm.querySelector('input[name="ibauthtoken"]').value;
+    const ibIBP = ibEditProForm.querySelector('input[name="ibibp"]').value;
+    const ibLocation = ibEditProForm.querySelector('input[name="iblocation"]').value;
+    const ibServices = ibEditProForm.querySelector('input[name="ibservices"]').value;
+    const ibWebsite = ibEditProForm.querySelector('input[name="ibwebsite"]').value;
+    const ibGitHub = ibEditProForm.querySelector('input[name="ibgithub"]').value;
+
+    fetch(ibEditProForm.action, {
+      method: ibEditProForm.method,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'text/html',
+        'ib-uid': ibUID,
+        'ib-authtoken': ibAuthToken
+      },
+      body: JSON.stringify({ 'ibuid': ibUID, 'ibauthtoken': ibAuthToken, 'ibibp': ibIBP, 'iblocation': ibLocation, 'ibservices': ibServices, 'ibwebsite': ibWebsite, 'ibgithub': ibGitHub })
+    })
+      .then(async response => {
+        for (let [key, value] of response.headers.entries()) {
+          console.log(`${key}: ${value}`);
+        }
+
+        await response.text();
+      })
+      .then(data => generateIBFormSuccess(data))
+      .catch(error => generateIBFormMessageFailure('edit-pro-message', error))
+  });
+}
+
+function characterCounter(counter) {
+  const textFieldForThe = document.querySelector('input[name="forthe"]');
+  const charCountDiv = document.getElementById(`${counter}`);
+
+  textFieldForThe.addEventListener('input', (event) => {
+    const charCountForThe = event.target.value.length;
+    charCountDiv.textContent = charCountForThe + '/1024';
+    if (charCountForThe > 1000) {
+      charCountDiv.style.color = 'red';
+    } else {
+      charCountDiv.style.color = 'green';
+    }
+  });
+
+  textFieldForThe.addEventListener('focus', () => {
+    charCountDiv.textContent = '0/1024';
+  });
+
+  // character counter isBy:
+  const textFieldIsBy = document.querySelector('input[name="isby"]');
+
+  textFieldIsBy.addEventListener('input', (event) => {
+    const charCountIsBy = event.target.value.length;
+    charCountDiv.textContent = charCountIsBy + '/1024';
+    if (charCountIsBy > 1000) {
+      charCountDiv.style.color = 'red';
+    } else {
+      charCountDiv.style.color = 'green';
+    }
+  });
+
+  textFieldIsBy.addEventListener('focus', () => {
+    charCountDiv.textContent = '0/1024';
+  });
+
+  // character counter isWith:
+  const textFieldIsWith = document.querySelector('input[name="iswith"]');
+
+  textFieldIsWith.addEventListener('input', (event) => {
+    const charCountIsWith = event.target.value.length;
+    charCountDiv.textContent = charCountIsWith + '/1024';
+    if (charCountIsWith > 1000) {
+      charCountDiv.style.color = 'red';
+    } else {
+      charCountDiv.style.color = 'green';
+    }
+  });
+
+  textFieldIsWith.addEventListener('focus', () => {
+    charCountDiv.textContent = '0/1024';
+  });
+}
+
+function generateIBFormMessageSuccess(id, content) {
+  document.getElementById(`${id}`).innerHTML = `
+  <em class="success">${content}</em>`;
+}
+
+function generateIBFormMessageFailure(id, content) {
+  document.getElementById(`${id}`).innerHTML = `
+  <em class="failure">${content}</em>`;
+}
+
+function generateIBLoginFormSuccess(ibUser, ibUID, ibAuthToken) {
+  const headers = new Headers();
+  headers.append('ib-uid', ibUID);
+  headers.append('ib-authtoken', ibAuthToken);
+
+  const body = new URLSearchParams();
+  body.append('ibuid', ibUID);
+  body.append('ibauthtoken', ibAuthToken);
+
+  fetch(`https://${domain}/${ibUser}`, {
+    method: 'POST',
+    headers: headers,
+    body: body
+  })
+  .then(response => response.json())
+  .then(data => {
+    // console.log(data);
+    generateIBFormSuccess(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    // Handle the error...
+  });
+  attachEventListeners();
+}
+
+function generateIBFormSuccess(content) {
+  if (content !== undefined) {
+    document.documentElement.innerHTML = content;
+  }
+  attachEventListeners();
+}
