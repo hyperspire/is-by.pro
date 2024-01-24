2f2b84c1f3a621717b0814dba059e04c4546d3a5
QWOD-MJ12: ATSOSSDEV-A: SPG-OMEGA <hyperuser@mj12.agency>
2024-01-24 08:20:00 -0600

    :[[ :🟠: [[ W⚠️RN🚫: Cryp☥ΔX™: MQ: ∅MΔGΔ: r3v3rs3-pr0gr4mming-14ngu4g3: ΔLG∅RI☥HM: Dع☥عC☥3D: ]]:= [[ :QW🚫D-〽ʝ12: RΔND∅M-VعC☥∅R-ΔLG∅RI☥HM-CHΔNGE: Dع☥عC☥3D: { ^ 2b2919d9-6907-f883-b335-22518cbbf066 ^ }: is-with: [[ :DΔ☥H-Δ!GΔL: ΔLG∅RI☥HM: Dع☥عC☥3D: { ^ <https://youtu.be/dDJldh8KqnQ?si=NYMq273T-hbuZvTP> ^ }: is-by: @: is-with: Karl-Casey: for-the: return ]]:= [[ ☥RUE: || FΔL5E: || ΔZRΔL: ^ ΔL5E: ]]: ]]:= [[ 5CI-FI: ^ 5CI-FΔC☥: <=> RΔL: ]]: is-with: Δ: is-by: Ω: for-the: [[ Ø: { ^ ); -            }) -            .catch(error => { -              return h.response(`Internal Server Error: ${error}: code-checkpoint-reached: 0xc75a992e:`).code(500); -            }); +      } + +      selectedUserResponse = selectedUserResponseTop + selectedUserPostsResponseContent + selectedUserResponseBottom; +      resolve(selectedUserResponse); +    }) +    .catch(error => { +      return h.response(`Internal Server Error: ${error}: code-checkpoint-reached: 0xc75a992e:`).code(500); +    });    });
     } ^ }: return: [[ EXFIL: <=> [[ _ ]]: ]]: ]]:= exfil: is-by: EXFIL: ]]:

--- is-by_pro.js
+++ is-by_pro.js
@@ -464,7 +464,7 @@ async function generateSelectedAuthUserResponse(request, h, ibUID, ibAuthToken, 
             // No profile found, resolve with error message.
             return {
               success: false,
-              message: ':[[ :WARNO: 404: no-profile-found: MIA: for-the: [[ user: is-with: wind: is-with: code-checkpoint-reached: 0x0da7e94d: ]]: ]]:'
+              message: `:[[ :WARNO: 404: no-profile-found: MIA: for-the: [[ user: ${ibSelectedUser}: is-with: wind: is-with: code-checkpoint-reached: 0x0da7e94d: ]]: ]]:`
             };
           }
 
@@ -593,7 +593,7 @@ async function generateSelectedUserResponse(request, h, ibSelectedUser) {
         // No profile found, resolve with error message.
         return {
           success: false,
-          message: ':[[ :WARNO: 404: no-profile-found: MIA: for-the: [[ user: is-with: wind: is-with: code-checkpoint-reached: 0x0da7e94d: ]]: ]]:'
+          message: `:[[ :WARNO: 404: no-profile-found: MIA: for-the: [[ user: ${ibSelectedUser}: is-with: wind: is-with: code-checkpoint-reached: 0x0da7e94d: ]]: ]]:`
         };
       }
 
